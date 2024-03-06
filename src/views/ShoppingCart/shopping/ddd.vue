03ded552b78b374a2081df679f332689cd55cfea
周应超 <1324853580@qq.com>
2024-03-06 08:56:47 +0800

    更新

---
+++ src/views/ShoppingCart/shopping/ddd.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+interface Props {
+  isTable: boolean
+}
+
+const props = defineProps<Props>()
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
+interface User {
+  date: string
+  name: string
+  address: string
+}
+
+const search = ref('')
+const filterTableData = computed(() =>
+    tableData1.filter(
+        (data) =>
+            !search.value ||
+            data.name.toLowerCase().includes(search.value.toLowerCase())
+    )
+)
+const handleEdit = (index: number, row: User) => {
+  console.log(index, row)
+}
+const handleDelete = (index: number, row: User) => {
+  console.log(index, row)
+}
+
+const tableData1: User[] = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'John',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Morgan',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Jessy',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
+</script>
+
+<template>
+  <el-table v-if="props.isTable === 'large'" :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180"/>
+    <el-table-column prop="name" label="Name" width="180"/>
+    <el-table-column prop="address" label="Address"/>
+  </el-table>
+  <el-table v-if="props.isTable === 'default'" :data="filterTableData" style="width: 100%">
+    <el-table-column label="Date" prop="date" />
+    <el-table-column label="Name" prop="name" />
+    <el-table-column align="right">
+      <template #header>
+        <el-input v-model="search" size="small" placeholder="Type to search" />
+      </template>
+      <template #default="scope">
+        <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
+        >Edit</el-button
+        >
+        <el-button
+            size="small"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+        >Delete</el-button
+        >
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/ShoppingCart/shopping/myApp.vue
+++ src/views/ShoppingCart/shopping/myApp.vue
@@ -1,8 +1,51 @@
 <template>
-    我的
+  <el-radio-group v-model="size" label="size control">
+    <el-radio-button label="large">large</el-radio-button>
+    <el-radio-button label="default">default</el-radio-button>
+  </el-radio-group>
+  <el-card>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="Approved by">
+        <el-input v-model="formInline.user" placeholder="Approved by" clearable />
+      </el-form-item>
+      <el-form-item label="Activity zone">
+        <el-select
+            v-model="formInline.region"
+            placeholder="Activity zone"
+            clearable
+        >
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Activity time">
+        <el-date-picker
+            v-model="formInline.date"
+            type="date"
+            placeholder="Pick a date"
+            clearable
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Query</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+<ddd :is-table="size"></ddd>
 </template>
 <script lang="ts" setup>
+import {ref,reactive} from 'vue'
+import ddd from './ddd.vue'
+const size = ref<'default' | 'large' | 'small'>('default')
+const formInline = reactive({
+  user: '',
+  region: '',
+  date: '',
+})
 
+const onSubmit = () => {
+  console.log('submit!')
+}
 </script>
-    
+
 <style lang="scss" scoped></style>
